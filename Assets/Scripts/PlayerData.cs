3e8b8f6a1e46286634e03e49bb4bec0b9bbe3bfc
TK17250 <TXK285@gmail.com>
2023-10-07 22:54:37 +0700

    Format Code PlayerData

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -19,63 +19,71 @@ using Firebase.Extensions;
 #endif
 
 
-    // Start is called before the first frame update
-    [Serializable]
-    public class PlayerData : MonoBehaviour
+// Start is called before the first frame update
+[Serializable]
+public class PlayerData : MonoBehaviour
+{
+    //  public uint PlayerId { 
+    //     get => playerId;
+    //     set => playerId = value;
+    // }
+    // [SerializeField]
+    // private uint playerId;
+
+    public uint Score
     {
-        //  public uint PlayerId { 
-        //     get => playerId;
-        //     set => playerId = value;
-        // }
-        // [SerializeField]
-        // private uint playerId;
+        get => score;
+        set => score = value;
+    }
+    
+    [SerializeField] private uint score;
 
-        public uint Score 
-        { 
-            get => score;
-            set => score = value;
-        }
-        [SerializeField] private uint score;
+    public string Time
+    {
+        get => time;
+        set => time = value;
+    }
 
-        public string Time
-        { 
-            get => time;
-            set => time = value;
-        }
-        [SerializeField] private string time;
-        // public PlayerData()
-        // {
-        //     // var random = new System.Random((int) DateTime.Now.Ticks & 0x0000FFFF);
-        //     // PlayerId = (uint) random.Next();
-        //     PlayerId = 999;
-        // }
+    [SerializeField] private string time;
+    // public PlayerData()
+    // {
+    //     // var random = new System.Random((int) DateTime.Now.Ticks & 0x0000FFFF);
+    //     // PlayerId = (uint) random.Next();
+    //     PlayerId = 999;
+    // }
+
     public void SendProfileToServer()
-        {
-            Debug.Log("Clicked "+this);
+    {
+        Debug.Log("Clicked " + this);
 
 #if UNITY_WEBGL || !UNITY_EDITOR
-            string jsonData = JsonUtility.ToJson(this);
-            FirebaseWebGLBridge.FirebaseFirestore.AddElementInArrayField("user-score", "Doc-score", "score", jsonData, gameObject.name, "DisplayInfo", "DisplayErrorObject");
-            Debug.Log("Added data to the Player document in the Gameplay collection. : "+this);
+        string jsonData = JsonUtility.ToJson(this);
+        FirebaseWebGLBridge.FirebaseFirestore.AddElementInArrayField("user-score", "Doc-score", "score", jsonData, gameObject.name, "DisplayInfo", "DisplayErrorObject");
+        Debug.Log("Added data to the Player document in the Gameplay collection. : " + this);
+
 #endif
-        }
-        // public override string ToString()
-        // {
-        //     var stringBuilder = new StringBuilder();
-        //     stringBuilder.Append("PlayerId = " + playerId);
-        //     stringBuilder.Append(" - Score = " + score);
-        //     stringBuilder.Append(" - Acc = " + acc);
-        //     return stringBuilder.ToString();
-        // }
-        public void DisplayInfo(string info){
-            Debug.Log(info);
-        }
-        public void DisplayErrorObject(string error)
-        {
-            var parsedError = FirebaseWebGLUtils.StringSerializationAPI.Deserialize(typeof(FirebaseWebGLObjects.FirebaseError), error) as FirebaseWebGLObjects.FirebaseError;
-            Debug.Log(parsedError.message);
-        }
     }
+
+    // public override string ToString()
+    // {
+    //     var stringBuilder = new StringBuilder();
+    //     stringBuilder.Append("PlayerId = " + playerId);
+    //     stringBuilder.Append(" - Score = " + score);
+    //     stringBuilder.Append(" - Acc = " + acc);
+    //     return stringBuilder.ToString();
+    // }
+
+    public void DisplayInfo(string info)
+    {
+        Debug.Log(info);
+    }
+
+    public void DisplayErrorObject(string error)
+    {
+        var parsedError = FirebaseWebGLUtils.StringSerializationAPI.Deserialize(typeof(FirebaseWebGLObjects.FirebaseError), error) as FirebaseWebGLObjects.FirebaseError;
+        Debug.Log(parsedError.message);
+    }
+}
 
 
 
